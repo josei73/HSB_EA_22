bac7ffa458cb0458e9b0b4a6d65ce9e61c1711ed
Henning Mohrmann <Mohrmann@Outlook.com>
2023-02-16 18:30:02 +0100

    +added a convex-hull representing an island -some cosmetic changes

--- src/main/resources/static/js/graph.js
+++ src/main/resources/static/js/graph.js
@@ -1,4 +1,4 @@
-let svg, svgWrapper, linksWrapper, nodesWrapper, axisWrapper, distanceWrapper;
+let svg, svgWrapper, linksWrapper, nodesWrapper, axisWrapper, distanceWrapper, hullWrapper;
 let xAxis, yAxis;
 let xScale, yScale;
 let data = {};
@@ -45,6 +45,7 @@ function initGraph() {
         .attr("viewBox", `0 0 ${attrs.svgWidth} ${attrs.svgHeight}`)
         .classed("svg-content-responsive", true);
 
+    hullWrapper = svg.append('g').attr("class", 'hull-wrapper');
     svgWrapper = svg.append('g').attr("class", 'svg-wrapper');
     axisWrapper = svg.append('g').attr("class", 'axis-wrapper');
     linksWrapper = svgWrapper.append('g').attr("class", 'links-wrapper');
@@ -61,7 +62,6 @@ function initGraph() {
     renderGraph({});
 }
 
-//TODO draw a geo map if an instance has GEO data
 function renderGraph(selection) {
     console.log("renderGraph function called")
     let nodes, links, numberOfNodes, tour;
@@ -73,6 +73,7 @@ function renderGraph(selection) {
     linksWrapper.selectAll(".link-group").remove();
     distanceWrapper.selectAll(".distance-link-group").remove();
     axisWrapper.selectAll("g").remove();
+    hullWrapper.selectAll("polygon").remove();
 
     if (!$.isEmptyObject(selection)) {
         let model = data.find(d => d.name === selection);
@@ -132,24 +133,37 @@ function updateGraph(nodeArray, linkArray, distanceArray) {
         .attr("transform", `translate(${attrs.margin.left}, 0)`)
         .call(yAxis);
 
+    // ########################### ISLAND ##########################
+    const hull = hullWrapper.append("polygon")
+        .attr("points", polygonString(convexHull(nodeArray)))
+
+    // d3.xml("./svg/topography.svg").then(function(xml) {
+    //     let pattern = d3.select(xml).select("pattern#topography");
+    //     console.log(xml)
+    //     console.log(pattern)
+    //     hull.attr("fill", "url(#" + pattern.attr("id") + ")");
+    // });
+
     //########################### GRAPH #########################
     //########################### NODES #########################
     const nodes = nodesWrapper.selectAll(".node-group").data(nodeArray);
     const nodesEntering = nodes.enter().append("g")
         .attr("class", "node-group")
 
-    const radius = rescaleNodeSize(nodeArray);
     nodesEntering.append("circle")
         .attr("cx", d => xScale(d.x))
         .attr("cy", d => yScale(d.y))
-        .attr("r", radius)
-        .attr("stroke", nodeArray.length < 1000 ? "black" : "none")
+        .attr("r", rescaleNodeSize(nodeArray.length))
+        .style("fill", nodeArray.length < 500 ? "white" : "black")
+        .style("stroke", nodeArray.length < 500 ? "black" : "white")
+        .style("stroke-width", rescaleNodeSize(nodeArray.length) / 10)
 
-    nodesEntering.append("text")
-        .text(d => d.id)
-        .attr("opacity", nodeArray.length < 100 ? 1 : 0)
-        .attr("x", d => xScale(d.x))
-        .attr("y", d => yScale(d.y) + 3);
+    if(nodeArray.length < 100) {
+        nodesEntering.append("text")
+            .text(d => d.id)
+            .attr("x", d => xScale(d.x))
+            .attr("y", d => yScale(d.y) + 3);
+    }
 
     //########################### LINKS #########################
     const links = linksWrapper.selectAll(".link-group").data(linkArray);
@@ -161,6 +175,7 @@ function updateGraph(nodeArray, linkArray, distanceArray) {
         .attr("y1", d => yScale(d.source.y))
         .attr("x2", d => xScale(d.target.x))
         .attr("y2", d => yScale(d.target.y))
+        .style("stroke-width", nodeArray.length < 100 ? 2 : 1)
 
     //############## DISTANCE LINKS (FORCE GRAPH ONLY)) ##############
     const distanceLinks = distanceWrapper.selectAll(".distance-link-group").data(distanceArray);
@@ -212,8 +227,12 @@ function updateGraph(nodeArray, linkArray, distanceArray) {
 // ######### ZOOM FUNCTIONS ###########
 function onZoom(event) {
     attrs.transform = event.transform;
+
     svg.style("background-size", `${attrs.transform.k * 20}%`);
+    svg.style("background-position", `${attrs.transform.x}px ${attrs.transform.y}px`)
+
     svgWrapper.attr("transform", attrs.transform);
+    hullWrapper.attr("transform", attrs.transform);
 
     let new_xScale = event.transform.rescaleX(xScale);
     let new_yScale = event.transform.rescaleY(yScale);
@@ -224,6 +243,33 @@ function onZoom(event) {
 function onZoomReset() {
     svg.transition().duration(500)
         .call(behaviours.zoom.transform, d3.zoomIdentity.translate(attrs.margin.left, attrs.margin.bottom).scale(0.9));
+}
+
+// ############# UI FUNCTIONS ##################
+function populateMetrics(tour) {
+    $("#costInput").val(tour.cost);
+    $("#timeInput").val(setTime(tour.time));
+}
+
+function setTime(time) {
+    if (time === undefined) return "";
+    if (time < 1000) {
+        return time + " ns"
+    } else if (time < 1000000) {
+        return time/1000 + " \u03BC"+"s"
+    } else if (time < 1000000000) {
+        return time / 1000000 + " ms"
+    } else {
+        return (time / 1000000000) + " s"
+    }
+}
+
+// ################ HELPER FUNCTIONS #####################
+function rescaleNodeSize(length) {
+    length = 50/(length/10)
+    if (length < .25) return .5
+    if (length > 7) return 7
+    return length
 }
 
 function normalize(value, min, max) {
@@ -252,31 +298,54 @@ function getMaximum(nodeArray) {
     }
 }
 
-function populateMetrics(tour) {
-    $("#costInput").val(tour.cost);
-    $("#timeInput").val(setTime(tour.time));
-}
+function convexHull(points) {
+    // Step 1: Sort the points lexicographically
+    points.sort(function(a, b) {
+        return a.x - b.x || a.y - b.y;
+    });
 
-function setTime(time) {
-    if (time === undefined) return "";
-    if (time < 1000) {
-        return time + " ns"
-    } else if (time < 1000000) {
-        return time/1000 + " \u03BC"+"s"
-    } else if (time < 1000000000) {
-        return time / 1000000 + " ms"
-    } else {
-        return (time / 1000000000) + " s"
+    // Step 2: Compute the lower hull
+    var lower = [];
+    for (var i = 0; i < points.length; i++) {
+        while (lower.length >= 2 &&
+        cross(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0) {
+            lower.pop();
+        }
+        lower.push(points[i]);
     }
+
+    // Step 3: Compute the upper hull
+    var upper = [];
+    for (var i = points.length - 1; i >= 0; i--) {
+        while (upper.length >= 2 &&
+        cross(upper[upper.length - 2], upper[upper.length - 1], points[i]) <= 0) {
+            upper.pop();
+        }
+        upper.push(points[i]);
+    }
+
+    // Step 4: Remove duplicate points
+    upper.pop();
+    lower.pop();
+
+    // Step 5: Combine the lower and upper hulls
+    return lower.concat(upper);
 }
 
-function rescaleNodeSize(nodeArray) {
-    let radius = 10;
-    if (nodeArray.length > 50) radius = 5;
-    if (nodeArray.length > 100) radius = 3;
-    if (nodeArray.length > 500) radius = 2;
-    if (nodeArray.length > 1000) radius = 1;
-    return radius;
+// Helper function to compute the cross product of three points
+function cross(a, b, c) {
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+function polygonString(points) {
+    let polygon = "";
+    for (let i = 0; i < points.length; i++) {
+        polygon += xScale(points[i].x) + "," + yScale(points[i].y);
+        if (i < points.length - 1) {
+            polygon += ",";
+        }
+    }
+    return polygon
 }
 
 loadData();
